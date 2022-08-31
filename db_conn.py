f410b3b2ba24008e445ed1707f2d1352aea72ec4
tjgran01 <tjgran01@syr.edu>
2022-08-31 15:29:02 -0400

    updated db_conn

--- db_conn.py
+++ db_conn.py
@@ -1,5 +1,3 @@
 import pyodbc
 
-print(pyodbc.drivers())
-
-_conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:syn-syr-dev-001.sql.azuresynapse.net,1433;Database=synsyrpooldev;Uid=xxx-xxx;Pwd=xxx-xxxEncrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryIntegrated;")
+_conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:syn-syr-dev-001.sql.azuresynapse.net,1433;Database=synsyrpooldev;Uid=xxx-xxx;Pwd=xxx-xxx;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryIntegrated;")
